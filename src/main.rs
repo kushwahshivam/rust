0166873ff61b413cb0e4cfd4886b113934b6d474
Shivam Kushwah <107871751+kushwahshivam@users.noreply.github.com>
2025-01-19 14:16:37 +0000

    new

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
 fn main() {
-    let n = fibo(4);
-    let m = is_even(20);
-    println!("{}",n);
-    println!("{}",m);
+    // let n = fibo(4);
+    // let m = is_even(20);
+    // let x = is_odd(3);
+    // println!("{}",n);
+    // println!("{}",m);
+    // println!("{}",x);
     
+    let name = String::from("shivam");
+    print!("{}",name);
 
-    let name:&str = "shivam";
-    println!("{}",name);    
+     
 }
 
 
 
-fn fibo(num : i32)->i32{
-    let mut first = 0;
-    let mut second =1;
-    if num==0{
-        return first;
-    }
-    if num==1{
-        return 1;
-    }
-    for _ in 1..num-1{
-        let temp = second;
-        second = second + first;
-        first= temp;
-    }
-    return second;
-}
+// fn fibo(num : i32)->i32{
+//     let mut first = 0;
+//     let mut second = 1;
+//         if num == 0{
+//             return first;
+//         }
+//         if num == 1{
+//             return 1;
+//         }
+//     for _ in 1 .. num-1{
+//         let  temp = second ;
+//         second = first + second;
+//         first = temp;
 
-fn is_even(num : i32)->bool{
-    if num%2==0{
-        return true;
-    }
-    return false;
-}
+//     }
+//     return second;
+// }
+
+// fn is_even(num : i32)->bool{
+//     if num%2==0{
+//         return true;
+//     }
+//     return false;
+// }
+
+// fn is_odd(num : i32)->bool{
+//     if num%2==0 {return false;}
+//     return  true;
+
+// }
